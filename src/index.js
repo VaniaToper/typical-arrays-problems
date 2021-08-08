63432601fa4f85a5c7cd0e2734a64eb129efd0c2
VaniaToper <vanjaqwe58@gmail.com>
2021-08-08 22:23:48 +0200

    feat🔨: completed task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+const validateArray = (array, callback) => {
+    if (!array || array.length <= 0) {
+        return 0;
+    }
+    return callback();
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+const min = (array) => {
+    return validateArray(array, () => {
+        return array.sort((a, b) => a - b)[0];
+    });
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+const max = (array) => {
+    return validateArray(array, () => {
+        return array.sort((a, b) => b - a)[0];
+    });
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+const avg = (array) => {
+    return validateArray(array, () => {
+        const newArr = array.reduce((previousValue, currentValue) => {
+            return previousValue + currentValue;
+        }, 0);
+        return newArr / array.length;
+    });
+};
+
+module.exports = {
+    min,
+    max,
+    avg,
+};
